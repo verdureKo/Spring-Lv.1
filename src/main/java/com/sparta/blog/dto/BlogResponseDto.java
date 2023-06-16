e2067b65c60c12925aa5a494141a210cad78779b
verdureKo <verdure.ko@gmail.com>
2023-06-16 16:56:43 +0900

    조회화면 비밀번호삭제

--- src/main/java/com/sparta/blog/dto/BlogResponseDto.java
+++ src/main/java/com/sparta/blog/dto/BlogResponseDto.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 public class BlogResponseDto {
     private Long id;
     private String author;
-    private String password;
+//    private String password;
     private String title;
     private String contents;
     private LocalDateTime createAt;
@@ -19,7 +19,7 @@ public class BlogResponseDto {
     public BlogResponseDto(Blog blog) {
         this.id = blog.getId();
         this.author = blog.getAuthor();
-        this.password = blog.getPassword();
+//        this.password = blog.getPassword();
         this.title = blog.getTitle();
         this.contents = blog.getContents();
         this.createAt = blog.getCreatedAt();
