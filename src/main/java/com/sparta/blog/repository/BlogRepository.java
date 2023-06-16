b8e1f45a0a606702ae0e36049056f9a979b6df47
verdureKo <verdure.ko@gmail.com>
2023-06-16 12:28:58 +0900

    핳..

--- src/main/java/com/sparta/blog/repository/BlogRepository.java
+++ src/main/java/com/sparta/blog/repository/BlogRepository.java
@@ -7,8 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BlogRepository extends JpaRepository<Blog, Long> {
-    List<Blog> findAllByOrderByModifiedAtDesc();
-//    List<Blog> findAllByUsername(String username);
-    List<Blog> findAllByContentsContainsOrderByModifiedAtDesc(String keyword);  // 콘텐츠의 키워드로 글찾기 내림차순정렬 
+    List<Blog> findAllByOrderByModifiedAtDesc(); // 콘텐츠 내림차순정렬
+    List<Blog> findAllByContentsContainsOrderByModifiedAtDesc(String keyword);  // 콘텐츠의 키워드로 글찾기 내림차순정렬
 
 }
